040cc335be9c2a9ab82bc902dd6c861a1a84edd1
Taras <info@animissimo.com>
2019-08-09 20:21:42 +0300

    Creating an effect controllers

---
+++ LiveAnimationTest/Assets/Scripts/EffectController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EffectController : MonoBehaviour
+{
+
+    public ParticleSystem effect;
+    public static ParticleSystem _effect;
+
+    void Start()
+    {
+        _effect = effect;
+    }
+
+    public static void StartEmit()
+    {
+        _effect.Emit(1);
+    }
+}
--- LiveAnimationTest/Assets/Scripts/Run_bSound.cs
+++ LiveAnimationTest/Assets/Scripts/Run_bSound.cs
@@ -4,7 +4,7 @@ public class Run_bSound : StateMachineBehaviour
 {
 	public AudioClip run;
 
-	void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
 		AudioSource.PlayClipAtPoint(run, animator.transform.position, 1);
 	}
